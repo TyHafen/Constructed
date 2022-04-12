ad8a3095d24f53db13092b3f22e2701c89e3c0c9
TyHafen <tyhafen@gmail.com>
2022-04-12 15:25:37 -0600

    Company

---
+++ Constructed/Services/CompaniesService.cs
@@ -0,0 +1,26 @@
+
+namespace Constructed.Services
+{
+    public class CompaniesService
+    {
+        internal Company GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<Company> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Company Create(object companyData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
